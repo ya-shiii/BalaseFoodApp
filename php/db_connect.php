b5eff4d0600453410f7c881899ae31c070941075
Joshua Vicente <josh.vicente00@gmail.com>
2024-05-26 09:44:58 +0800

    ready

--- php/db_connect.php
+++ php/db_connect.php
@@ -2,9 +2,9 @@
 
 // Database credentials
 $servername = "localhost";
-$username = "root"; //u663034616_balase
-$password = ""; //j9&NyKQ&s
-$dbname = "balasefoodapp"; //u663034616_balasefoodapp
+$username = "u663034616_balase"; 
+$password = "j9&NyKQ&s"; 
+$dbname = "u663034616_balasefoodapp"; 
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
